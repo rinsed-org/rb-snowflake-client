a551186a5a83ab6b01d4259441dc417f1bb4a3a3
Reid Nimz <reid@rinsed.co>
2023-11-21 17:28:24 -0600

    add date and real type conversions, time still seems to not match before, not sure which is right

--- lib/ruby_snowflake/row.rb
+++ lib/ruby_snowflake/row.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require "date"
 require "time"
 
 module RubySnowflake
   class Row
+    EPOC_JULIAN_DAY_NUMBER = Date.new(1970,1,1).jd
+
     def initialize(row_types, column_to_index, data)
       @row_types = row_types
       @data = data
@@ -24,6 +27,10 @@ module RubySnowflake
         Time.at(timestamp.to_f - offset_minutes.to_i * 60)
       when :boolean
         @data[index] == "true"
+      when :date # TODO looks like we might be returning a Time in the go version
+        @data[index] = Date.jd(@data[index].to_i + EPOC_JULIAN_DAY_NUMBER)
+      when :real
+        @data[index] = @data[index].to_f
       else
         @data[index]
       end
