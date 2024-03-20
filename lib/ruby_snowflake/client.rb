8b3afb7ad4f1713de8a4d78e946f08c879cfb2d3
Reid Nimz <reid@rinsed.co>
2024-03-20 14:05:14 -0500

    pass an exponential backoff to retryable.retry

--- lib/ruby_snowflake/client.rb
+++ lib/ruby_snowflake/client.rb
@@ -190,6 +190,7 @@ module RubySnowflake
         request.body = body unless body.nil?
 
         Retryable.retryable(tries: @http_retries + 1,
+                            sleep: lambda {|n| 2**n }, # 1, 2, 4, 8, etc
                             on: [RetryableBadResponseError, OpenSSL::SSL::SSLError],
                             log_method: retryable_log_method) do
           response = nil
