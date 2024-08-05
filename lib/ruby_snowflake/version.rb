77748f1475141fede5648a9689aed81652ed3007
Reid Nimz <reid@rinsed.co>
2024-08-05 14:41:41 -0500

    don't switch version yet, want to pull in dependency changes to master first

--- lib/ruby_snowflake/version.rb
+++ lib/ruby_snowflake/version.rb
@@ -1,3 +1,3 @@
 module RubySnowflake
-  VERSION = "1.1.2"
+  VERSION = "1.1.1"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,10 +5,8 @@ require "dotenv/load"
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
   config.order = "random"
   config.mock_with( :rspec ) do |mock|
     mock.syntax = :expect
   end
 end
-
